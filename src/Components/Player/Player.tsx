54f8373aef9575cb22c4fe87fd5aa46448f6e147
dh1-Omarchy <237053497+Samz6293@users.noreply.github.com>
2026-09-06 22:35:04 +0600

    Update Player component Finalize design and add responsiveness

--- src/Components/Player/Player.tsx
+++ src/Components/Player/Player.tsx
@@ -9,41 +9,42 @@ const Player = ({player}: PlayerProps) => {
     <>
         {/* card border */}
         <div className="flex flex-col content-box bg-linear-40 
-        from-[#cc0000bb] from-1% to-[#9197a521]  to-60%
-        rounded-xl p-4 my-2 max-w-5xl gap-4">
-
+        from-[#cc0000de] from-1% to-[#180d04]  to-60%
+        rounded-xl p-4 my-4 max-w-5xl gap-5
+        sm:p-8 md:p-10">
             {/* top row {position, number, status} */}
-            <div className="flex justify-between text-xs">
-                <div className="flex justify-between gap-1.5">
-                  <p className="px-2 bg-red-800 rounded-md">{player.strPosition}</p>
-                  <p className="px-3 bg-slate-700 rounded-sm"># {player.strNumber ? player.strNumber : "NA"}</p>
-                </div>
-                {/* <p className="px-4 bg-emerald-100 rounded-full text-emerald-700">Status: {player.strStatus}</p> */}
+            <div className="flex justify-between text-xs items-center
+            sm:text-sm md:text-lg">
+                  <p className="px-2 bg-linear-60 from-red-600 to-red-800 rounded-md font-black">{player.strPosition}</p>
+                  <p className="px-3 bg-slate-900 border-2 border-gray-700 rounded-sm"># {player.strNumber ? player.strNumber : "NA"}</p>
             </div>
 
             {/* id name team nationality photo */}
             <div className="flex justify-between items-center">
               <div>
-                <p className="text-xs">#{player.idPlayer}</p>
-                <p className="text-xl">{firtstName}</p>
-                <p className="text-xl">{lastname}</p>
-                <p className="text-xs">{player.dateBorn} | {player.strNationality}</p>
+                <p className="text-xs font-light text-gray-500">#{player.idPlayer}</p>
+                <p className="text-2xl font-extrabold font-anybody
+                sm:text-4xl md:text-5xl lg:text-6xl">{firtstName}</p>
+                <p className="text-xl text-red-600 font-rainbow font-black 
+                sm:text-4xl md:text-5xl lg:text-6xl">{lastname}</p>
+                <p className="text-xs text-gray-200">{player.dateBorn} | {player.strNationality}</p>
               </div>
                 <img src={player.strThumb} className="rounded-lg inline-block w-[45%]" alt="" />
             </div>
 
             {/* weight age foot */}
             <div className="flex justify-around
-            bg-slate-700 rounded-lg p-2 text-xs">
+            bg-gray-900 rounded-lg p-2 text-xs items-center">
 
               <div className="flex flex-col items-center">
-                <p>weight</p>
-                <p>{player.strWeight}</p>
+                <p className="text-slate-400">weight</p>
+                <p className="font-bold">{player.strWeight}</p>
               </div>
-
+              <hr className=" w-10 h-0.5 rotate-90 bg-slate-700 rounded-lg border-0"/>
+              {/* <p className="px-4 bg-emerald-100 rounded-full text-emerald-700">Status: {player.strStatus}</p> */}
               <div className="flex flex-col items-center">
-                <p>foot</p>
-                <p>{player.strSide ? player.strSide : "both"}</p>
+                <p className="text-slate-400">foot</p>
+                <p className="font-bold">{player.strSide ? player.strSide : "both"}</p>
               </div>
 
             </div>
